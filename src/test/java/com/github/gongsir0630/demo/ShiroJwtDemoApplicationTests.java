99e33ff107e4c0fd36e0b8a2e5f91bb200e94c5c
码之泪殇 <3198307775@qq.com>
2021-03-21 20:00:29 +0800

    :star: init app

---
+++ src/test/java/com/github/gongsir0630/demo/ShiroJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gongsir0630.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroJwtDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
